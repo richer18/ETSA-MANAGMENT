49af09b93fe98c02a397e4569568b33e2ff1a337
richer18 <richeralanano@gmail.com>
2025-06-16 16:58:39 +0800

    ✨ Updated .gitignore and modified App, package files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Routers from "./Router/Router";
 
+// Define your theme with linearGradient functionality
+const theme = createTheme({
+  palette: {
+      primary: { main: '#1976d2' },
+      secondary: { main: '#dc004e' },
+      gradients: {
+          primary: {
+              main: '#1976d2',
+              state: '#0d47a1', // Darker shade for gradient state
+          },
+          dark: {
+              main: '#000',
+              state: '#333',
+          },
+      },
+  },
+  typography: {
+      // Define your typography settings
+  },
+  functions: {
+      linearGradient: (color1, color2) => `linear-gradient(${color1}, ${color2})`,
+  },
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+        <Routers />
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default App
