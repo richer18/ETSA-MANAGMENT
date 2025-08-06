d6a4d2ac5e012b60cfc1904b63d44ac5d13f4fe3
richer18 <richeralanano@gmail.com>
2025-08-06 09:01:15 +0800

    Auto-commit on 2025-08-06 09:01:15

--- src/FRONTEND/components/BUSINESS/BusinessRegistration/components/BNew.jsx
+++ src/FRONTEND/components/BUSINESS/BusinessRegistration/components/BNew.jsx
@@ -1,6 +1,5 @@
 import { Box, Step, StepButton, Stepper } from "@mui/material";
 import React from "react";
-import { AddressProvider } from "./AddressContext"; // <-- import your context provider
 import BusinessAdditional from "./BusinessAdditional";
 import BusinessAddress from "./BusinessAdress";
 import BusinessForm from "./BusinessFormNew";
@@ -17,25 +16,64 @@ const steps = [
 function BNew() {
   const [activeStep, setActiveStep] = React.useState(0);
 
-  const handleNext = () => setActiveStep((prevStep) => prevStep + 1);
-  const handleBack = () => setActiveStep((prevStep) => prevStep - 1);
-  // const handleStep = (step) => () => setActiveStep(step);
+  // Manage ALL form data in parent state
+  const [formData, setFormData] = React.useState({
+    businessInfo: {},
+    businessAddress: {},
+    businessOperation: {},
+    businessAdditional: {},
+  });
+
+  const handleNext = () => setActiveStep((prev) => prev + 1);
+  const handleBack = () => setActiveStep((prev) => prev - 1);
+
+  // Update handler for any form section
+  const updateFormData = (section, data) => {
+    setFormData((prev) => ({
+      ...prev,
+      [section]: {
+        ...prev[section],
+        ...data,
+      },
+    }));
+  };
 
   const renderStepContent = () => {
     switch (activeStep) {
       case 0:
-        return <BusinessForm handleNext={handleNext} />;
+        return (
+          <BusinessForm
+            data={formData.businessInfo}
+            updateData={(data) => updateFormData("businessInfo", data)}
+            handleNext={handleNext}
+          />
+        );
       case 1:
         return (
-          <BusinessAddress handleNext={handleNext} handleBack={handleBack} />
+          <BusinessAddress
+            data={formData.businessAddress}
+            updateData={(data) => updateFormData("businessAddress", data)}
+            handleNext={handleNext}
+            handleBack={handleBack}
+          />
         );
       case 2:
         return (
-          <BusinessOperation handleNext={handleNext} handleBack={handleBack} />
+          <BusinessOperation
+            data={formData.businessOperation}
+            updateData={(data) => updateFormData("businessOperation", data)}
+            handleNext={handleNext}
+            handleBack={handleBack}
+          />
         );
       case 3:
         return (
-          <BusinessAdditional handleNext={handleNext} handleBack={handleBack} />
+          <BusinessAdditional
+            data={formData.businessAdditional}
+            updateData={(data) => updateFormData("businessAdditional", data)}
+            handleNext={handleNext}
+            handleBack={handleBack}
+          />
         );
       case 4:
         return <div>Review & Submit Component</div>;
@@ -48,24 +86,16 @@ function BNew() {
     <Box sx={{ width: "100%", mt: 4 }}>
       <Box sx={{ display: "flex", justifyContent: "center" }}>
         <Stepper nonLinear activeStep={activeStep} sx={{ width: "80%" }}>
-          {steps.map((label, index) => (
+          {steps.map((label) => (
             <Step key={label}>
               <StepButton color="inherit">{label}</StepButton>
             </Step>
           ))}
         </Stepper>
       </Box>
-
       {renderStepContent()}
     </Box>
   );
 }
 
-// Wrap BNew in AddressProvider
-export default function BNewWrapper() {
-  return (
-    <AddressProvider>
-      <BNew />
-    </AddressProvider>
-  );
-}
+export default BNew;
